777a19fb0e56076c59d3116533b14bc347c3f00b
kabradshaw1 <itllbok82@gmail.com>
2022-09-13 16:42:52 -0400

    9.3.5

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js')
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+    ]);
+  };
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+  promptUser().then(answers => console.log(answers));
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });

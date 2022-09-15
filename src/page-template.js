d79876047673a5470baa68e77efb0562f51f9889
kabradshaw1 <itllbok82@gmail.com>
2022-09-15 09:32:36 -0400

    done with 9.4

--- src/page-template.js
+++ src/page-template.js
@@ -12,6 +12,7 @@ const generateAbout = aboutText => {
   `;
 };
 
+// create the projects section
 const generateProjects = projectsArr => {
   return `
     <section class="my-3" id="portfolio">
@@ -25,7 +26,7 @@ const generateProjects = projectsArr => {
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
               Built With:
-              ${languages.join(', ')}
+              ${languages.map(language => language).join(',')}
             </h5>
             <p>${description}</p>
             <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
@@ -55,22 +56,7 @@ const generateProjects = projectsArr => {
   `;
 };
 
-const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
-  ({ name, description, languages, link }) => {
-    return `
-      <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-        <h3 class="portfolio-item-title text-light">${name}</h3>
-        <h5 class="portfolio-languages">
-          Built With:
-          ${languages.join(', ')}
-        </h5>
-        <p>${description}</p>
-        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-      </div>
-    `;
-  }
-);
-
+// export function to generate entire page
 module.exports = templateData => {
   // destructure page data by section
   const { projects, about, ...header } = templateData;
@@ -78,7 +64,6 @@ module.exports = templateData => {
   return `
   <!DOCTYPE html>
   <html lang="en">
-
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -88,7 +73,7 @@ module.exports = templateData => {
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
   </head>
-
+  
   <body>
     <header>
       <div class="container flex-row justify-space-between align-center py-3">
